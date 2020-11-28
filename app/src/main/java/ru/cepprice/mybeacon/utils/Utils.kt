6f9f2722bd2c8a7b782ad9db0cbee4b3d262734b
cepprice <super.cepprice@gmail.com>
2020-11-29 00:44:27 +0300

    Add new util functions

--- app/src/main/java/ru/cepprice/mybeacon/utils/Utils.kt
+++ app/src/main/java/ru/cepprice/mybeacon/utils/Utils.kt
@@ -4,13 +4,26 @@ import org.altbeacon.beacon.Beacon
 
 object Utils {
 
-    fun getNewBeacons(prev: MutableCollection<Beacon>, curr: MutableCollection<Beacon>)
-    : List<Beacon> = curr.filter { currBeacon -> !prev
-                .map { it.id1 to it.id2 to it.id3 }
-                .contains(currBeacon.id1 to currBeacon.id2 to currBeacon.id3)
+    fun generateRandomUuid(): String =
+        "${randomHexFourDigitNumber()}${randomHexFourDigitNumber()}-" +
+                "${randomHexFourDigitNumber()}-" +
+                "${randomHexFourDigitNumber()}-" +
+                "${randomHexFourDigitNumber()}-" +
+                "${randomHexFourDigitNumber()}${randomHexFourDigitNumber()}${randomHexFourDigitNumber()}"
+
+    fun getLostBeacons(prev: MutableCollection<Beacon>, curr: MutableCollection<Beacon>)
+    : List<Beacon> = prev.filterNot { prevBeacon -> curr
+        .map { it.id1 to it.id2 to it.id3 }
+        .contains(prevBeacon.id1 to prevBeacon.id2 to prevBeacon.id3)
     }
 
-    fun addSavingOrder(
+    fun getNewBeacons(prev: MutableCollection<Beacon>, curr: MutableCollection<Beacon>)
+    : List<Beacon> = curr.filterNot { currBeacon -> prev
+        .map { it.id1 to it.id2 to it.id3 }
+        .contains(currBeacon.id1 to currBeacon.id2 to currBeacon.id3)
+    }
+
+    fun addBeaconSavingOrder(
         srcBeacons: ArrayList<Beacon>,
         newBeacons: List<Beacon>
     ): ArrayList<Beacon> {
@@ -29,5 +42,44 @@ object Utils {
         }
         return srcBeacons
     }
+
+    fun quickSortBeacons(arr: ArrayList<Beacon>, left: Int = 0, right: Int = arr.size - 1)
+    : ArrayList<Beacon> {
+        if (arr.isEmpty()) return ArrayList()
+
+        var res = arr
+
+        var l = left
+        var r = right
+        val pivot = res[(left + right) / 2]
+
+        while (l <= r) {
+            while (res[l].distance < pivot.distance) l++
+            while (res[r].distance > pivot.distance) r--
+
+            if (l <= r) {
+                res[l] = res[r].also { res[r] = res[l] }
+                l++
+                r--
+            }
+        }
+
+        val index = l
+        if (left < index - 1) res = quickSortBeacons(res, left, index - 1)
+        if (index < right) res = quickSortBeacons(res, index, right)
+
+        return res
+    }
+
+    private fun randomHexDigit(): String {
+        val chars = "1234567890ABCDEF"
+        return chars[(0..15).random()].toString()
+    }
+
+    private fun randomHexFourDigitNumber(): String =
+        randomHexDigit()
+            .plus(randomHexDigit())
+            .plus(randomHexDigit())
+            .plus(randomHexDigit())
 
 }
