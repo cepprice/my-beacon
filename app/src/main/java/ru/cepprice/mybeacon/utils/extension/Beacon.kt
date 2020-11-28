66dfba97dad016ab67f347b0a26b13cdaa8ce360
cepprice <super.cepprice@gmail.com>
2020-11-28 20:10:04 +0300

    Add extension for Beacon

---
+++ app/src/main/java/ru/cepprice/mybeacon/utils/extension/Beacon.kt
@@ -0,0 +1,14 @@
+package ru.cepprice.mybeacon.utils.extension
+
+import org.altbeacon.beacon.Beacon
+import ru.cepprice.mybeacon.data.BeaconView
+
+fun Beacon.toBeaconView(): BeaconView = with(this) {
+    BeaconView(
+        uuid = id1.toString(),
+        major = id2.toString(),
+        minor = id3.toString(),
+        rssi = "$rssi dBm",
+        distance = "%.2f m".format(distance)
+    )
+}
