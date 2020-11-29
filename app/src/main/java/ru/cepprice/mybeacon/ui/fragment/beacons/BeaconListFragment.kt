98c5ccefbcf1bad8f693c50a494c91862b8a51c5
cepprice <super.cepprice@gmail.com>
2020-11-29 23:22:22 +0300

    Implement bluetooth state changed listener

--- app/src/main/java/ru/cepprice/mybeacon/ui/fragment/beacons/BeaconListFragment.kt
+++ app/src/main/java/ru/cepprice/mybeacon/ui/fragment/beacons/BeaconListFragment.kt
@@ -1,8 +1,7 @@
 package ru.cepprice.mybeacon.ui.fragment.beacons
 
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.bluetooth.BluetoothAdapter
+import android.content.*
 import android.os.Bundle
 import android.os.RemoteException
 import android.util.Log
@@ -12,7 +11,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import org.altbeacon.beacon.*
+import ru.cepprice.mybeacon.R
+import ru.cepprice.mybeacon.data.receiver.BluetoothStateChangeListener
+import ru.cepprice.mybeacon.data.receiver.BluetoothStateChangeNotifier
 import ru.cepprice.mybeacon.databinding.FragmentBeaconListBinding
 import ru.cepprice.mybeacon.ui.fragment.main.MainFragmentDirections
 import ru.cepprice.mybeacon.utils.TimedBeaconSimulator
@@ -20,18 +23,21 @@ import ru.cepprice.mybeacon.utils.autoCleared
 import ru.cepprice.mybeacon.utils.extension.quickSort
 import ru.cepprice.mybeacon.utils.extension.toBeaconView
 
-class BeaconListFragment : Fragment(), BeaconConsumer, RangeNotifier {
+class BeaconListFragment : Fragment(), BeaconConsumer, RangeNotifier, BluetoothStateChangeNotifier {
 
     companion object {
         val region1 = Region("Region1", null, null, null)
     }
 
-
     private var binding: FragmentBeaconListBinding by autoCleared()
 
-    private lateinit var adapter: BeaconListAdapter
+    private lateinit var beaconListAdapter: BeaconListAdapter
+    private var snackbar: Snackbar? = null
+
+    private lateinit var bluetoothAdapter: BluetoothAdapter
 
     private lateinit var beaconManager: BeaconManager
+    private lateinit var receiver: BroadcastReceiver
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,7 +51,46 @@ class BeaconListFragment : Fragment(), BeaconConsumer, RangeNotifier {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        adapter = BeaconListAdapter{
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        registerReceiver()
+        setupRecyclerView()
+        setupBeaconManager()
+
+        if (bluetoothAdapter.isEnabled) {
+            startScanningForBeacons()
+        } else {
+            onBluetoothDisabled()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stopScanningForBeacons()
+        requireActivity().unregisterReceiver(receiver)
+        beaconManager.unbind(this)
+    }
+
+    override fun onBluetoothDisabled() {
+        stopScanningForBeacons()
+
+        if (snackbar == null) {
+            snackbar = Snackbar.make(
+                binding.rvBeacons,
+                getString(R.string.message_main_turn_on_bluetooth),
+                Snackbar.LENGTH_INDEFINITE)
+        }
+
+        if (!snackbar!!.isShown) snackbar!!.show()
+    }
+
+    override fun onBluetoothEnabled() {
+        snackbar?.dismiss()
+        startScanningForBeacons()
+    }
+
+    private fun setupRecyclerView() {
+        beaconListAdapter = BeaconListAdapter{
             val action = MainFragmentDirections.actionMainFragmentToBeaconInfoDialog(arrayOf(
                 it.uuid, it.major, it.minor, it.rssi, it.distance
             ))
@@ -54,18 +99,43 @@ class BeaconListFragment : Fragment(), BeaconConsumer, RangeNotifier {
         }
 
         binding.rvBeacons.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvBeacons.adapter = adapter
+        binding.rvBeacons.adapter = beaconListAdapter
+    }
 
+    private fun setupBeaconManager() {
         BeaconManager.setBeaconSimulator(TimedBeaconSimulator())
         beaconManager = BeaconManager.getInstanceForApplication(requireContext())
-//        beaconManager.beaconParsers
-//            .add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
+
+        beaconManager.beaconParsers
+            .add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
+        beaconManager.beaconParsers
+            .add(BeaconParser().setBeaconLayout(BeaconParser.ALTBEACON_LAYOUT))
+        beaconManager.beaconParsers
+            .add(BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT))
+        beaconManager.beaconParsers
+            .add(BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT))
+        beaconManager.beaconParsers
+            .add(BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_URL_LAYOUT))
+
         beaconManager.bind(this)
     }
 
-    override fun onStop() {
-        super.onStop()
-        beaconManager.unbind(this)
+    private fun registerReceiver() {
+        receiver = BluetoothStateChangeListener(this)
+        val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        requireActivity().registerReceiver(receiver, filter)
+    }
+
+    private fun startScanningForBeacons() {
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(region1)
+            beaconManager.startRangingBeaconsInRegion(region1)
+        } catch (e: RemoteException) {}
+    }
+
+    private fun stopScanningForBeacons() {
+        beaconManager.stopMonitoringBeaconsInRegion(region1)
+        beaconManager.stopRangingBeaconsInRegion(region1)
     }
 
     override fun onBeaconServiceConnect() {
@@ -88,17 +158,12 @@ class BeaconListFragment : Fragment(), BeaconConsumer, RangeNotifier {
                 beaconManager.addRangeNotifier(this@BeaconListFragment)
             }
         })
-
-        try {
-            beaconManager.startMonitoringBeaconsInRegion(region1)
-            beaconManager.startRangingBeaconsInRegion(region1)
-        } catch (e: RemoteException) {}
     }
 
     override fun didRangeBeaconsInRegion(beacons: MutableCollection<Beacon>?, region: Region?) {
         if (beacons == null) return
 
-        adapter.updateBeacons(beacons.toList().quickSort().map { it.toBeaconView() })
+        beaconListAdapter.updateBeacons(beacons.toList().quickSort().map { it.toBeaconView() })
     }
 
     override fun getApplicationContext(): Context = requireContext()
--- app/src/main/java/ru/cepprice/mybeacon/ui/fragment/devices/DeviceListFragment.kt
+++ app/src/main/java/ru/cepprice/mybeacon/ui/fragment/devices/DeviceListFragment.kt
@@ -1,32 +1,37 @@
 package ru.cepprice.mybeacon.ui.fragment.devices
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
+import android.content.BroadcastReceiver
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import ru.cepprice.mybeacon.R
+import ru.cepprice.mybeacon.data.receiver.BluetoothStateChangeListener
+import ru.cepprice.mybeacon.data.receiver.BluetoothStateChangeNotifier
 import ru.cepprice.mybeacon.databinding.FragmentDeviceListBinding
 import ru.cepprice.mybeacon.utils.autoCleared
 import ru.cepprice.mybeacon.utils.extension.mapToDeviceView
 
-class DeviceListFragment() : Fragment() {
+class DeviceListFragment() : Fragment(), BluetoothStateChangeNotifier {
 
     private var binding: FragmentDeviceListBinding by autoCleared()
 
-    private val bluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var leScanCallback: ScanCallback
+    private var bluetoothLeScanner: BluetoothLeScanner? = null
+
+    private lateinit var receiver: BroadcastReceiver
+
 
     lateinit var deviceListAdapter: DeviceListAdapter
-
-    private val leScanCallback: ScanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            super.onScanResult(callbackType, result)
-            deviceListAdapter.addDevice(result.mapToDeviceView())
-        }
-    }
+    private var snackbar: Snackbar? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,11 +46,34 @@ class DeviceListFragment() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        deviceListAdapter = DeviceListAdapter()
-        with(binding.rvBleDevices) {
-            layoutManager = LinearLayoutManager(requireContext())
-            adapter = deviceListAdapter
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        setupScanCallback()
+        registerReceiver()
+
+        if (bluetoothAdapter.isEnabled) {
+            bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+        } else {
+            onBluetoothDisabled()
         }
+
+        setupRecyclerView()
+    }
+
+    override fun onBluetoothDisabled() {
+        if (snackbar == null) {
+            snackbar = Snackbar.make(
+                binding.rvBleDevices,
+                getString(R.string.message_main_turn_on_bluetooth),
+                Snackbar.LENGTH_INDEFINITE)
+        }
+
+        if (!snackbar!!.isShown) snackbar!!.show()
+    }
+
+    override fun onBluetoothEnabled() {
+        snackbar?.dismiss()
+        if (bluetoothLeScanner == null) bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+        if (bluetoothAdapter.isEnabled) startScanningForLe()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -64,21 +92,46 @@ class DeviceListFragment() : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        startScanningForLe()
+        if (bluetoothAdapter.isEnabled) startScanningForLe()
+        else onBluetoothDisabled()
     }
 
     override fun onStop() {
         super.onStop()
+        requireActivity().unregisterReceiver(receiver)
         stopScanningForLe()
     }
 
+    private fun registerReceiver() {
+        receiver = BluetoothStateChangeListener(this)
+        val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        requireActivity().registerReceiver(receiver, filter)
+    }
+
+    private fun setupRecyclerView() {
+        deviceListAdapter = DeviceListAdapter()
+        with(binding.rvBleDevices) {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = deviceListAdapter
+        }
+    }
+
+    private fun setupScanCallback() {
+        leScanCallback = object : ScanCallback() {
+            override fun onScanResult(callbackType: Int, result: ScanResult) {
+                super.onScanResult(callbackType, result)
+                deviceListAdapter.addDevice(result.mapToDeviceView())
+            }
+        }
+    }
+
     private fun startScanningForLe() {
-        bluetoothLeScanner.startScan(leScanCallback)
+        bluetoothLeScanner?.startScan(leScanCallback)
         Log.d("M_DeviceListFragment", "Started scanning for BLE devices")
     }
 
     private fun stopScanningForLe() {
-        bluetoothLeScanner.stopScan(leScanCallback)
+        bluetoothLeScanner?.stopScan(leScanCallback)
         Log.d("M_DeviceListFragment", "Stopped scanning for BLE devices")
     }
 
