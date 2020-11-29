47b5520a512c89d67dc57d059e708259b9ffec6e
cepprice <super.cepprice@gmail.com>
2020-11-29 17:54:51 +0300

    Fix to define name of device without explicit one

--- app/src/main/java/ru/cepprice/mybeacon/utils/extension/ScanResult.kt
+++ app/src/main/java/ru/cepprice/mybeacon/utils/extension/ScanResult.kt
@@ -1,8 +1,21 @@
 package ru.cepprice.mybeacon.utils.extension
 
 import android.bluetooth.le.ScanResult
+import android.util.Log
 import ru.cepprice.mybeacon.data.DeviceView
 
 fun ScanResult.mapToDeviceView(): DeviceView {
-    return DeviceView(this.device.name, this.device.address, "${this.rssi} dBm")
+    val name = this.device.name.let {
+        if (it != null) {
+            return@let it
+        }
+
+        val manufacturerData = this.scanRecord?.manufacturerSpecificData
+        val bytes = manufacturerData?.get(6) ?: return@let "None"
+        val reg = Regex("[a-zA-z0-9-]+")
+        val lastInvalid = bytes.indexOfLast { byte ->  !reg.containsMatchIn(byte.toChar().toString()) }
+        return@let bytes.decodeToString(lastInvalid + 1)
+    }
+
+    return DeviceView(name, this.device.address, "${this.rssi} dBm")
 }
